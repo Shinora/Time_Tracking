a94d4c78b49a0b6ca339f97a91ef0fc0a013d765
simon <simonamato23@gmail.com>
2021-12-16 21:24:16 +0100

    idle immage correction

--- utils.py
+++ utils.py
@@ -116,6 +116,7 @@ class Screen():
     \'._' 30 '_.'/
      '-._'--'_.-'
          `""` """
-        self.draw.text((self.x, self.top+0), clock, font=self.tinyfont, fill=255)
+        text = " --------- IDLE MODE IS ON ----------- "
+        self.draw.text((self.x, self.top+0), text, font=self.font, fill=255)
         self.disp.image(self.image)
         self.disp.show()
